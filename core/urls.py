5a262c00971f74f0d78305ce2875198515b05489
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2023-03-22 21:29:18 +0600

    some updates

--- core/urls.py
+++ core/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
+
+    path('inbox/', ListThreads.as_view(), name='inbox'),
+    path('inbox/create-thread/', CreateThread.as_view(), name='create-thread'),
+    path('inbox/<int:pk>/', ThreadView.as_view(), name='thread'),
+    path('inbox/<int:pk>/create-message/', CreateMessage.as_view(), name='create-message'),
 ]
