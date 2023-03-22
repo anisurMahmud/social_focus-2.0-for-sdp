889f2a618b5f4522edddb329dd84c75e482c3bf6
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2023-03-22 22:05:41 +0600

    Revert "Merge branch 'main' into fixing-mistake-2"
    This reverts commit 99f77541071b0458db9f58fde0d362d201428354, reversing
    changes made to 0b992112cef9253c55f78964d7bdaca5a799b4c0.

--- social_focus/settings.py
+++ social_focus/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,6 +126,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
